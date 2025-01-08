f841c6898e2873c45ea1dd06c46258259af70123
thaoanhhaa1 <thaoanhhaa1@gmail.com>
2025-01-08 09:23:37 +0700

    Routes setup

---
+++ src/routes/index.ts
@@ -0,0 +1,11 @@
+import Home from '../pages/Home';
+import SignIn from '../pages/SignIn';
+
+const publicRoutes = [
+    { path: '/', component: Home },
+    { path: '/sign-in', component: SignIn },
+];
+
+const privateRoutes = [];
+
+export { publicRoutes, privateRoutes };
