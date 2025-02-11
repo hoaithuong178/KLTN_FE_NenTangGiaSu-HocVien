5cae07f0de174f60ff8b88d1e3755698d179cebb
Hoai Thuong <thefightgirl178@gmail.com>
2025-02-11 22:27:32 +0700

    Register UI

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,17 +1,23 @@
 import Home from '../pages/Home';
 import NotFound from '../pages/NotFound';
+import NotifySuccess from '../pages/NotifySuccess';
+import VerifyOTP from '../pages/OTP';
+import Register from '../pages/Register';
 import SignIn from '../pages/SignIn';
 
 export interface IRoute {
     path: string;
-    component: () => JSX.Element;
-    layout?: () => JSX.Element;
+    component: React.FC;
+    layout?: React.FC;
 }
 
 const publicRoutes: Array<IRoute> = [
     { path: '/', component: Home },
     { path: '/sign-in', component: SignIn },
     { path: '*', component: NotFound },
+    { path: '/register', component: Register },
+    { path: '/verify-otp', component: VerifyOTP },
+    { path: '/notify-success', component: NotifySuccess },
 ];
 
 const privateRoutes: Array<IRoute> = [];
