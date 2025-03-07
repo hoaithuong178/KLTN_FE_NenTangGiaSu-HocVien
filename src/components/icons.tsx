eadbcad55ff712cfdff7993dc3f2ff81d93f5c3a
Nguyen Thi Hoai Thuong <85344825+hoaithuong178@users.noreply.github.com>
2025-03-08 01:53:01 +0700

    Update icons.tsx

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -340,6 +340,23 @@ export const SearchIcon = ({ className }: { className?: string }) => (
 );
 
 export const UploadIcon = ({ className }: { className?: string }) => (
+    <svg
+        className={className}
+        xmlns="http://www.w3.org/2000/svg"
+        width={24}
+        height={24}
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth="1.5"
+        stroke="currentColor"
+    >
+        <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5m-13.5-9L12 3m0 0 4.5 4.5M12 3v13.5"
+        />
+    </svg>
+);
 
 export const StarIcon = ({ className }: { className?: string }) => (
 
@@ -362,8 +379,20 @@ export const StarIcon = ({ className }: { className?: string }) => (
     </svg>
 );
 export const DownloadIcon = ({ className }: { className?: string }) => (
-
-            d="M12 3l2.09 6.26L21 9.27l-5 4.87L17.18 21 12 17.77 6.82 21l1.09-6.86-5-4.87 6.91-1.01L12 3z"
+    <svg
+        className={className}
+        xmlns="http://www.w3.org/2000/svg"
+        width={24}
+        height={24}
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth="1.5"
+        stroke="currentColor"
+    >
+        <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5M16.5 12 12 16.5m0 0L7.5 12m4.5 4.5V3"
         />
     </svg>
 );
