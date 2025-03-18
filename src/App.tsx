bfef820432e0e9e1a8e56fd8cf44a5228a83fa34
thaoanhhaa1 <thaoanhhaa1@gmail.com>
2025-03-18 16:13:03 +0700

    Refactor routing and implement code-splitting for improved performance
    - Wrapped Routes in a Suspense component to handle loading states.
    - Converted route components to lazy-loaded imports for better performance and reduced initial load time.
    - Updated route mapping to use path as the key for better consistency.

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,34 @@
+import { Suspense } from 'react';
 import { HelmetProvider } from 'react-helmet-async';
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import { Fragment } from 'react/jsx-runtime';
+import LoadingComponent from './components/LoadingComponent';
 import { publicRoutes } from './routes';
 
 function App() {
     return (
         <HelmetProvider>
             <Router>
-                <Routes>
-                    {publicRoutes.map((route, index) => {
-                        const Page = route.component;
-                        const Layout = route.layout || Fragment;
+                <Suspense fallback={<LoadingComponent />}>
+                    <Routes>
+                        {publicRoutes.map((route) => {
+                            const Page = route.component;
+                            const Layout = route.layout || Fragment;
 
-                        return (
-                            <Route
-                                key={index}
-                                path={route.path}
-                                element={
-                                    <Layout>
-                                        <Page />
-                                    </Layout>
-                                }
-                            />
-                        );
-                    })}
-                </Routes>
+                            return (
+                                <Route
+                                    key={route.path}
+                                    path={route.path}
+                                    element={
+                                        <Layout>
+                                            <Page />
+                                        </Layout>
+                                    }
+                                />
+                            );
+                        })}
+                    </Routes>
+                </Suspense>
             </Router>
         </HelmetProvider>
     );
